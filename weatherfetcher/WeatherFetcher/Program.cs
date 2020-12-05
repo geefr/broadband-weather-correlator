7bbf5f5fd49fc9fbf4f3532461a508707e0de75d
Gareth Francis <gfrancis.dev@gmail.com>
2020-12-05 13:51:32 +0000

    Initial Commit, executable setup

---
+++ weatherfetcher/WeatherFetcher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace WeatherFetcher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // TODO: Some startup/validation stuff goes here
+
+      // Polling interval (seconds)
+      var pollingInterval = 10;
+
+      // Start processing, and run until killed
+      while(true)
+      {
+        update();
+        Thread.Sleep(pollingInterval * 1000);
+      }
+    }
+
+    static void update()
+    {
+      Console.WriteLine("Hello, this is an update from your friendly local weatherman");
+    }
+  }
+}
