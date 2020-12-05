c193803ee82865ce1337407ecfb6ba130f54a1ca
Gareth Francis <gfrancis.dev@gmail.com>
2020-12-05 15:47:16 +0000

    Fetch values from rainfall api

--- Weatherfetcher/WeatherFetcher.Tests/EnvAgencyRainfallTests.cs
+++ Weatherfetcher/WeatherFetcher.Tests/EnvAgencyRainfallTests.cs
@@ -8,11 +8,28 @@ namespace WeatherFetcher.Tests
     public class EnvAgencyRainfallTests
     {
         [Fact]
-        public void TestListStations()
+        public void TestReadStationValues()
         {
             var api = new EnvAgencyRainfall();
-            var stations = api.GetStations();
+            var stations = api.GetStations(51.0899, -0.4547, 20);
             Assert.NotNull(stations);
+
+            foreach( var s in stations.items )
+            {
+                if( s.measures == null || s.measures.Count == 0 ) continue;
+                var rainfallMeasure = s.measures.Find(x => x.parameter.Equals("rainfall") );
+                if( rainfallMeasure == null ) continue;
+
+                var measurement = api.GetLatestMeasurement(s.stationReference);
+                if( measurement.items != null && measurement.items.Count > 0 )
+                {
+                    foreach( var i in measurement.items )
+                    {
+                        if( !i.parameter.Equals("rainfall") ) continue;
+                        Console.WriteLine($"{s.stationReference} : {s.lat},{s.lon} : {i.latestReading.value}{i.unitName}");
+                    }
+                }
+            }
         }
     }
 }
--- Weatherfetcher/WeatherFetcher/Services/EnvAgencyRainfall.cs
+++ Weatherfetcher/WeatherFetcher/Services/EnvAgencyRainfall.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using RestSharp;
 
 namespace WeatherFetcher.Services
@@ -10,26 +11,103 @@ namespace WeatherFetcher.Services
   public class EnvAgencyRainfall
   {
     private const string APIRoot = "http://environment.data.gov.uk/flood-monitoring";
-    private const string APIRainfallStations = "stations?parameter=rainfall";
-
     private RestClient Client = null;
-
-    public class Station
-    {
-      
-    }
 
     public EnvAgencyRainfall()
     {
       Client = new RestClient(APIRoot);
     }
 
-    public List<Station> GetStations()
+    /// <summary>
+    /// List rainfall stations within distance(km) of lat,lon(wgs84) 
+    /// </summary>
+    public GetStationsData GetStations(double lat, double lon, int distance)
     {
-      var req = new RestRequest(APIRainfallStations, DataFormat.Json);
-      var res = Client.Get<List<Station>>(req);
+      var req = new RestRequest($"id/stations?parameter=rainfall&lat={lat}&long={lon}&dist={distance}", DataFormat.Json);
+      var res = Client.Get<GetStationsData>(req);
+      if (!res.IsSuccessful) return null;
       return res.Data;
     }
 
+    /// <summary>
+    /// Read the latest measurement for a station
+    /// </summary>
+    /// <param name="stationReference">Station reference string, from GetStationData.Item.stationReference</param>
+    public GetLatestMeasurementData GetLatestMeasurement(string stationReference)
+    {
+      var req = new RestRequest($"id/stations/{stationReference}/measures", DataFormat.Json);
+      var res = Client.Get<GetLatestMeasurementData>(req);
+      if (!res.IsSuccessful) return null;
+      return res.Data;
+    }
+
+
+    public class StationMetaData
+    {
+      public string publisher { get; set; }
+      public string licence { get; set; }
+      public string documentation { get; set; }
+      public string version { get; set; }
+      public string comment { get; set; }
+      public List<string> hasFormat { get; set; }
+    }
+
+    public class GetStationsData
+    {
+      public StationMetaData meta { get; set; }
+
+      public class Item
+      {
+        public string easting { get; set; }
+        public string gridReference { get; set; }
+        public string label { get; set; }
+        public double lat { get; set; }
+        [JsonPropertyName("long")]
+        public double lon { get; set; }
+
+        public class Measure
+        {
+          public string parameter { get; set; }
+          public string parameterName { get; set; }
+          public double period { get; set; }
+          public string qualifier { get; set; }
+          public string unitName { get; set; } // mm, cm, m?
+        }
+        public List<Measure> measures {get; set;}
+        public string northing { get; set; }
+        public string notation { get; set; }
+        public string stationReference { get; set; }
+      }
+      public List<Item> items { get; set; }
+    }
+  
+    public class GetLatestMeasurementData
+    {
+      public StationMetaData meta {get;set;}
+
+      public class Item
+      {
+        public string label {get;set;}
+        public class Reading
+        {
+          public string date {get;set;}
+          public string dateTime {get;set;}
+          public string measure {get;set;}
+          public string value {get;set;}
+        }
+        public Reading latestReading {get;set;}
+        public string notation {get;set;}
+        public string parameter {get;set;}
+        public string parameterName {get;set;}
+        public string period{get;set;}
+        public string qualifier{get;set;}
+        public string station{get;set;}
+        public string stationReference{get;set;}
+        public string unit{get;set;}
+        public string unitName {get;set;}
+        public string valueType {get;set;}
+      }
+      public List<Item> items {get;set;}
+    }
   }
 }
